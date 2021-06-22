f3f9798d78e656cda204feffa5a6ea266fcc8d7a
LaScheggia <lascheggia605@gmail.com>
2021-06-22 14:24:28 +0200

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,26 @@
+<header class="container">
+    <!-- LOGO -->
+    <div class="head-logo">
+      <a href="/">
+        <img src="../assets/img/dc-logo.png" alt="logo png">
+      </a>
+    </div>
+
+  <!-- NAVBAR -->
+    <div class="head-nav">
+      <nav>
+        <ul>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Collectibles</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">Fans</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Shop</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
