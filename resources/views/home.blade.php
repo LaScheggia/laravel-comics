1fe7cc151fb54fa6e15ddc42a0b7a2bc0f0e2b78
LaScheggia <lascheggia605@gmail.com>
2021-06-22 17:49:58 +0200

    esercizio finito

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,16 @@
 @extends('layouts.main')
 
 @section('content')
-    <main>
+    <main class="container">
         <div class="fumetti">
             @foreach ($fumetti as $comics)
+            <div class="carta">
                 <img src="{{ $comics['thumb'] }}">
                 <p>
                 {{ $comics['title'] }}
                 </p>
+            </div>
+
             @endforeach
         </div>
     </main>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,71 @@
 <footer>
-    <h1>FOOTER</h1>
+    <div class="container up-fot"> <!-- qua c'è il menu con tutte le listozze infinite -->
+        <div class="listy">
+          <!-- listozze -->
+          <div> <!-- comics + shop -->
+            <h5>DC COMICS</h5>
+            <ul>
+              <li><a href="#">Characters</a></li>
+              <li><a href="#">Comics</a></li>
+              <li><a href="#">Movies</a></li>
+              <li><a href="#">TV</a></li>
+              <li><a href="#">Games</a></li>
+              <li><a href="#">Videos</a></li>
+              <li><a href="#">news</a></li>
+            </ul>
+            <h5>SHOP</h5>
+            <ul>
+              <li><a href="#">Shop DC</a></li>
+              <li><a href="#">Shop DC Collectibles</a></li>
+            </ul>
+          </div>
+          <div> <!-- DC -->
+            <h5>DC</h5>
+            <ul>
+              <li><a href="#">Terms Of Use</a></li>
+              <li><a href="#">Privacy Policy (New)</a></li>
+              <li><a href="#">Ad Choices</a></li>
+              <li><a href="#">Advertising</a></li>
+              <li><a href="#">Jobs</a></li>
+              <li><a href="#">Subscriptions</a></li>
+              <li><a href="#">Talent workshops</a></li>
+              <li><a href="#">CPSC certificates</a></li>
+              <li><a href="#">ratings</a></li>
+              <li><a href="#">shop help</a></li>
+              <li><a href="#">contact us</a></li>
+            </ul>
+          </div>
+          <div> <!-- SITES -->
+            <h5>SITES</h5>
+            <ul>
+              <li><a href="/">DC</a></li>
+              <li><a href="#">MAD Magazine</a></li>
+              <li><a href="#">DC kids</a></li>
+              <li><a href="#">DC Universe</a></li>
+              <li><a href="#">DC power visa</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="ft-logo">
+          <!-- logo storto dc x 2 -->
+        </div>
+      </div>
+      <div class="down-fot"> <!-- footerino micro con signup e i socials -->
+        <div class="container">
+        <div class="ft-but">
+          <button><a href="#">SIGN UP NOW!</a></button>
+        </div>
+        <div  class="socials"> <!-- socials -->
+          <span><h5>FOLLOW US</h5></span>
+          <ul>
+            <li><a href="#"><img src="{{ asset('css/img/footer-facebook.png') }}" alt=""></a></li>
+            <li><a href="#"><img src="{{ asset('css/img/footer-twitter.png') }}" alt=""></a></li>
+            <li><a href="#"><img src="{{ asset('css/img/footer-youtube.png') }}" alt=""></a></li>
+            <li><a href="#"><img src="{{ asset('css/img/footer-pinterest.png') }}" alt=""></a></li>
+            <li><a href="#"><img src="{{ asset('css/img/footer-periscope.png') }}" alt=""></a></li>
+          </ul>
+        </div>
+      </div>
+
+      </div>
 </footer>
